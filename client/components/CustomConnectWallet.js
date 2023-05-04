358b3215f954734aaea8427888436a9dcd0adeb4
theycallmeloki <dude.1996@live.com>
2023-05-04 14:01:11 +0530

    Update

--- client/components/CustomConnectWallet.js
+++ client/components/CustomConnectWallet.js
@@ -1,86 +1,80 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-export const CustomConnectWallet = ({ onWalletAddressChange }) => {
-  const [connectedAddress, setConnectedAddress] = useState(null);
+const withWalletButtonContent = (Component) => {
+  return ({ onWalletAddressChange, ...props }) => {
+    const handleAddressChange = useCallback(
+      (account) => {
+        if (account && account.address) {
+          onWalletAddressChange(account.address);
+          console.log('Connected wallet address:', account.address);
+        }
+      },
+      [onWalletAddressChange]
+    );
 
-  const handleAddressChange = useCallback(
-    (account) => {
-      if (account && account.address) {
-        setConnectedAddress(account.address);
-        onWalletAddressChange(account.address);
-        console.log('Connected wallet address:', account.address);
+    useEffect(() => {
+      handleAddressChange(props.account);
+    }, [props.account, handleAddressChange]);
+
+    const handleSignInClick = async () => {
+      try {
+        const connectedAccount = await props.openConnectModal();
+        if (connectedAccount) {
+          console.log('Wallet address:', connectedAccount.address);
+        }
+      } catch (error) {
+        console.error('Error connecting wallet:', error);
       }
-    },
-    [onWalletAddressChange]
-  );
+    };
 
+    return (
+      <Component
+        {...props}
+        handleSignInClick={handleSignInClick}
+        handleAddressChange={handleAddressChange}
+      />
+    );
+  };
+};
+
+const WalletButtonContent = withWalletButtonContent(
+  ({
+    account,
+    chain,
+    openAccountModal,
+    authenticationStatus,
+    mounted,
+    handleSignInClick,
+  }) => {
+    const ready = mounted && authenticationStatus !== 'loading';
+    const connected =
+      ready &&
+      account &&
+      chain &&
+      (!authenticationStatus || authenticationStatus === 'authenticated');
+
+    if (!connected) {
+      return (
+        <button onClick={handleSignInClick} type="button" style={{ fontSize: '20px' }}>
+          Sign in
+        </button>
+      );
+    }
+
+    return (
+      <button onClick={openAccountModal} type="button" style={{ fontSize: '20px' }}>
+        {account.displayName}
+        {account.displayBalance ? ` (${account.displayBalance})` : ''}
+      </button>
+    );
+  }
+);
+
+export const CustomConnectWallet = ({ onWalletAddressChange }) => {
   return (
     <ConnectButton.Custom>
-      {({
-        account,
-        chain,
-        openAccountModal,
-        openChainModal,
-        openConnectModal,
-        authenticationStatus,
-        mounted,
-      }) => {
-        useEffect(() => {
-          handleAddressChange(account);
-        }, [account, handleAddressChange]);
-
-        const ready = mounted && authenticationStatus !== 'loading';
-        const connected =
-          ready &&
-          account &&
-          chain &&
-          (!authenticationStatus ||
-            authenticationStatus === 'authenticated');
-
-        const handleSignInClick = async () => {
-          try {
-            const connectedAccount = await openConnectModal();
-            if (connectedAccount) {
-              console.log('Wallet address:', connectedAccount.address);
-            }
-          } catch (error) {
-            console.error('Error connecting wallet:', error);
-          }
-        };
-
-        return (
-          <div
-            {...(!ready && {
-              'aria-hidden': true,
-              'style': {
-                opacity: 0,
-                pointerEvents: 'none',
-                userSelect: 'none',
-              },
-            })}
-          >
-            {(() => {
-              if (!connected) {
-                return (
-                  <button onClick={handleSignInClick} type="button" style={{ fontSize: '20px' }}>
-                    Sign in
-                  </button>
-                );
-              }
-
-              return (
-                <div>
-                  <button onClick={openAccountModal} type="button" style={{ fontSize: '20px' }}>
-                    {account.displayName}
-                    {account.displayBalance ? ` (${account.displayBalance})` : ''}
-                  </button>
-                </div>
-              );
-            })()}
-          </div>
-        );
-      }}
+      {(props) => <WalletButtonContent {...props} onWalletAddressChange={onWalletAddressChange} />}
     </ConnectButton.Custom>
   );
 };
