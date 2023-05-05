08e90909b65a2f2e013748769cc5cd938e5c3c91
theycallmeloki <dude.1996@live.com>
2023-05-05 10:28:22 +0530

    Update

--- client/pages/api/nfts.js
+++ client/pages/api/nfts.js
@@ -7,17 +7,31 @@ const { INFURA_API_KEY, INFURA_SECRET_API_KEY } = process.env;
 export default async function handler(req, res) {
   try {
     const walletAddress = req.query.wallet;
-    const { data } = await axios.get(`https://nft.api.infura.io/networks/${chainId}/accounts/${walletAddress}/assets/nfts`, {
-      headers: {
-        Authorization: `Basic ${Buffer.from(`${INFURA_API_KEY}:${INFURA_SECRET_API_KEY}`).toString('base64')}`,
-      },
-    });
 
-    // const nftsInWallet = data.transfers.filter(transfer => transfer.to === walletAddress);
+    let allNFTs = [];
+    let nextCursor = '';
 
-    res.status(200).json(data);
+    const headers = {
+      Authorization: `Basic ${Buffer.from(`${INFURA_API_KEY}:${INFURA_SECRET_API_KEY}`).toString('base64')}`,
+    };
+
+    while (true) {
+      const { data } = await axios.get(`https://nft.api.infura.io/networks/${chainId}/accounts/${walletAddress}/assets/nfts?cursor=${nextCursor}`, {
+        headers,
+      });
+
+      allNFTs = allNFTs.concat(data.assets);
+
+      if (data.pagination && data.pagination.cursor) {
+        nextCursor = data.pagination.cursor;
+      } else {
+        break;
+      }
+    }
+
+    res.status(200).json({ assets: allNFTs });
   } catch (error) {
     console.error('Error fetching NFT transfers:', error);
-    res.status(500).json({ error: 'Failed to fetch NFTs', message: error.message  });
+    res.status(500).json({ error: 'Failed to fetch NFTs', message: error.message });
   }
 }
